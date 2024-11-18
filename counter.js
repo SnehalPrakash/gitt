011d7cf8bcb720e060d7e7f6433917773f8837db
Snehal Prakash <snehaldps@gmail.com>
2024-11-18 17:09:54 +0530

    random number generator

---
+++ counter.js
@@ -0,0 +1,23 @@
+const decrease=document.getElementById("decrease");
+const reset=document.getElementById("reset");
+const increase=document.getElementById("increase");
+const countlabel=document.getElementById("countlabel");
+
+let count=0;
+decrease.onclick=function(){
+    --count;
+countlabel.textContent=count;
+}
+increase.onclick= function(){
+    count++;
+countlabel.textContent=count;
+}
+reset.onclick= function(){
+    count=0;
+countlabel.textContent=count;
+}
+/*
+we got elements by their id , declared as constants
+declared a variabe count as 0 ,
+made function of each variable to get text content on click and display value,
+*/
